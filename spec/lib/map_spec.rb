d3b8c7d617e4ccbbb7026f44a07ff16b6d51b54c
Luc Donnet <luc.donnet@dryade.net>
2011-08-11 18:29:30 +0200

    Add spec for map

---
+++ spec/lib/map_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe MapLayers do
+
+  describe ".const_missing" do
+
+    it "should return the class MapLayers::Layer::Google for GOOGLE" do
+      MapLayers::GOOGLE.class.to_s.should == "OpenLayers::Layer::Google"
+    end
+    
+  end
+
+end
