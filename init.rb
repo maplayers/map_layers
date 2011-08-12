142f165f8f81da128567a71a2d013f53dba94156
Luc Donnet <luc.donnet@dryade.net>
2011-08-12 18:49:12 +0200

    Fix init.rb to use just map_layer.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,4 @@
-require 'openlayers'
 require 'map_layers'
-require 'map'
-require 'view_helpers'
-require 'js_wrapper'
 
 ActionController::Base.send(:include, MapLayers)
 ActionView::Base.send(:include, MapLayers)
