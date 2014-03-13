ea307c901944905c2ecd9c0b5d3a4ee2c1899113
kmmndr <kommander@laposte.net>
2014-03-13 10:38:39 +0100

    simple_map_handler cleanup

--- vendor/assets/javascripts/open_layers_handlers/simple_map_handler.js
+++ vendor/assets/javascripts/open_layers_handlers/simple_map_handler.js
@@ -4,9 +4,25 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
   this.controls = {};
   this.dragCallbacks = {};
 
+  /**
+   * Method: ajaxPopupContent
+   * Check whether or not ajax content has been defined for a feature popup.
+   *
+   * Parameters:
+   * feature - {feature} The feature to check.
+   */
   this.ajaxPopupContent = function(feature) {
     return (feature.attributes.popupContentUrl !== undefined);
   }
+
+  /**
+   * Method: addFeaturePopupContent
+   * Add static content for popup.
+   *
+   * Parameters:
+   * feature - {feature} The feature where popup is requested.
+   * popup - {popup} useless ?
+   */
   this.addFeaturePopupContent = function(feature, popup) {
     name = feature.attributes.name;
     url = feature.attributes.popupContentUrl;
@@ -37,6 +53,13 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * APIMethod: addFeaturePopup
+   * Open popup on feature.
+   *
+   * Parameters:
+   * feature - {feature} The feature where popup should be opened.
+   */
   this.addFeaturePopup = function(feature) {
     this.selectedFeature = feature;
 
@@ -72,6 +95,14 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
       this.addFeaturePopupContent(feature);
     }
   }
+
+  /**
+   * APIMethod: removeFeaturePopup
+   * Close popup on feature.
+   *
+   * Parameters:
+   * feature - {feature} The feature where popup should be closed.
+   */
   this.removeFeaturePopup = function(feature) {
     if(feature.popup) {
       this.map.removePopup(feature.popup);
@@ -79,16 +110,39 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
       delete feature.popup;
     }
   }
+
+  /**
+   * APIMethod: toggleFeaturePopup
+   * Toggle popup on feature.
+   *
+   * Parameters:
+   * feature - {feature} The feature where popup should be opened or closed.
+   */
   this.toggleFeaturePopup = function(feature) {
     if(feature.popup) { this.removeFeaturePopup(feature); }
     else { this.addFeaturePopup(feature); }
   }
 
+  /**
+   * APIMethod: toggleLayerFeaturePopup
+   * Toggle popup on feature by layer and feature number.
+   *
+   * Parameters:
+   * layer - {layer} The layer containing the requested feature.
+   * feature_id - {number} The feature number to toggle.
+   */
   this.toggleLayerFeaturePopup = function(layer, feature_id) {
     feature = this.getLayerFeatureById(layer, feature_id);
     this.toggleFeaturePopup(feature);
   }
 
+  /**
+   * APIMethod: toggleLayer
+   * Toggle layer visibility from layer name.
+   *
+   * Parameters:
+   * layerName - {string} The layer name to toggle.
+   */
   this.toggleLayer = function(layerName) {
     var layer = this.map.getLayersByName(layerName)[0];
     visible = layer.getVisibility();
@@ -100,12 +154,28 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     return layer.features.length;
   }
 
+  /**
+   * Method: getLayerFeatureByNb
+   * Get feature from layer name and feature number.
+   *
+   * Parameters:
+   * layerName - {string} The layer name where to search for feature.
+   * feature_nb - {number} The feature number.
+   */
   this.getLayerFeatureByNb = function(layerName, feature_nb) {
     var layer = this.map.getLayersByName(layerName)[0];
     var feature = feature_nb != -1 ? layer.features[feature_nb] : layer.features.slice(-1).pop();
     return feature;
   }
 
+  /**
+   * Method: getLayerFeatureById
+   * Get feature from layer name and feature id.
+   *
+   * Parameters:
+   * layerName - {string} The layer name where to search for feature.
+   * feature_id - {number} The feature id.
+   */
   this.getLayerFeatureById = function(layerName, feature_id) {
     var layer = this.map.getLayersByName(layerName)[0];
     var feature = null;
@@ -123,21 +193,54 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     return feature;
   }
 
+  /**
+   * APIMethod: getLonlatFromCoordinates
+   * Get lonlat from latitude and longitude.
+   *
+   * Parameters:
+   * lat - {float} Latitude.
+   * lon - {float} Longitude.
+   */
   this.getLonlatFromCoordinates = function(lat, lon) {
     var lonlat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
     return lonlat;
   }
 
+  /**
+   * APIMethod: setCenterOnLonlat
+   * Set map center on lonlat at specified zoom level.
+   *
+   * Parameters:
+   * lonlat - {LonLat} Lonlat object for coordinates.
+   * zoom - {number} Zoom level.
+   */
   this.setCenterOnLonlat = function(lonlat, zoom) {
     if (zoom === undefined) { zoom = this.map.getZoom(); }
     if (lonlat) { this.map.setCenter(lonlat, zoom); }
   }
 
+  /**
+   * APIMethod: setCenterOnCoordinates
+   * Set map center on coordinates at specified zoom level.
+   *
+   * Parameters:
+   * lat - {float} Latitude coordinate.
+   * lon - {float} Longitude coordinate.
+   * zoom - {number} Zoom level.
+   */
   this.setCenterOnCoordinates = function(lat, lon, zoom) {
     var lonlat = this.getLonlatFromCoordinates(lat, lon);
     this.setCenterOnLonlat(lonlat, zoom);
   }
 
+  /**
+   * APIMethod: setCenterOnFeature
+   * Set map center on feature at specified zoom level.
+   *
+   * Parameters:
+   * feature - {feature} Feature to center on.
+   * zoom - {number} Zoom level.
+   */
   this.setCenterOnFeature = function(feature, zoom) {
     if (feature != null)
     {
@@ -150,11 +253,28 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * APIMethod: setCenterOnFeatureByNb
+   * Set map center on feature at specified zoom level.
+   *
+   * Parameters:
+   * layerName - {string} The layer name where to search for feature.
+   * feature_nb - {number} The feature number.
+   * zoom - {number} Zoom level.
+   */
   this.setCenterOnFeatureByNb = function(layerName, feature_nb, zoom) {
     var feature = this.getLayerFeatureByNb(layerName, feature_nb)
     this.setCenterOnFeature(feature, zoom);
   }
 
+  /**
+   * APIMethod: setDragCallback
+   * Handle callback for events.
+   *
+   * Parameters:
+   * evt - {string} Event name.
+   * method - {function} Function to bind on event.
+   */
   this.setDragCallback = function(evt, method) {
     switch (evt) {
     case 'onEnter':
@@ -169,13 +289,31 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * Method: onFeatureSelect
+   * Callback function called on feature select.
+   */
   this.onFeatureSelect = function(event) {
     this.addFeaturePopup(event.feature);
   }
+
+  /**
+   * Method: onFeatureUnselect
+   * Callback function called when feature is unselected.
+   */
   this.onFeatureUnselect = function(event) {
     this.removeFeaturePopup(event.feature);
   }
 
+  /**
+   * APIMethod: initializeControls
+   * Initialize base controls (Select, Draw, Drag) and bind to callbacks
+   * functions.
+   *
+   * Parameters:
+   * controlsName - {string} Control group name.
+   * layersNames - {string/array} Layer/layers to use with this group of controls.
+   */
   this.initializeControls = function(controlsName, layersNames) {
     // controlsName OR layers attribute
     var layers = new Array();
@@ -244,6 +382,14 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * APIMethod: switchControl
+   * Select from base controls : Select, Draw or Drag
+   *
+   * Parameters:
+   * controlsName - {string} Control group name.
+   * ctrl - {string} Control type to use.
+   */
   this.switchControl = function(controlsName, ctrl) {
     for(key in this.controls[controlsName])
     {
@@ -256,6 +402,13 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * APIMethod: toggleControlObject
+   * Activate/Deactivate control
+   *
+   * Parameters:
+   * ctrl - {control} Control object to toggle.
+   */
   this.toggleControlObject = function(ctrl) {
     var active = ctrl.active;
     if (ctrl.active == true)
@@ -265,6 +418,13 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     return active;
   }
 
+  /**
+   * APIMethod: toggleControlByClass
+   * Activate/Deactivate control for control class.
+   *
+   * Parameters:
+   * klass - {string} Control class to toggle.
+   */
   this.toggleControlByClass = function(klass) {
     var active = false;
     var handler = this;
@@ -274,6 +434,10 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     return active;
   }
 
+  /**
+   * APIMethod: toggleControls
+   * Activate/Deactivate all controls.
+   */
   this.toggleControls = function() {
     var active = false;
 
@@ -286,6 +450,16 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     return active;
   }
 
+  /**
+   * APIMethod: addFeature
+   * Add a new feature on layer.
+   *
+   * Parameters:
+   * layerName - {string} The layer name on which to add feature.
+   * lat - {float} Latitude coordinate.
+   * lon - {float} Longitude coordinate.
+   * icon - {hash} additionnal feature parameters.
+   */
   this.addFeature = function(layerName, lat, lon, icon) {
     var layer = this.map.getLayersByName(layerName)[0];
 
@@ -304,6 +478,14 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     return feature;
   }
 
+  /**
+   * APIMethod: addFeatureAttributes
+   * Add attributes to feature.
+   *
+   * Parameters:
+   * feature - {feature} Feature.
+   * attributes - {hash} additionnal feature attributes.
+   */
   this.addFeatureAttributes = function(feature, attributes) {
     if (feature != null)
     {
@@ -313,6 +495,13 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * APIMethod: removeFeatures
+   * Remove all features from layer.
+   *
+   * Parameters:
+   * layerName - {string} The layer name on which to remove features.
+   */
   this.removeFeatures = function(layerName) {
     var layer = this.map.getLayersByName(layerName)[0];
 
@@ -323,6 +512,13 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     }
   }
 
+  /**
+   * APIMethod: destroyLayer
+   * Remove layer and associated objects.
+   *
+   * Parameters:
+   * layerName - {string} The layer name to remove.
+   */
   this.destroyLayer = function(layerName) {
     var layer = this.map.getLayersByName(layerName)[0];
 
@@ -351,55 +547,13 @@ OpenLayersHandlers.SimpleMapHandler = function(map) {
     layer.destroy();
   }
 
+  /**
+   * APIMethod: getMapCenter
+   * Get map center coordinates.
+   */
   this.getMapCenter = function() {
     var centerLonLat = this.map.getCenter().clone().transform( this.map.getProjectionObject(),new OpenLayers.Projection("EPSG:4326") );
     center = [centerLonLat.lat,centerLonLat.lon];
     return center;
   }
 };
-
-
-
-
-// map_handler.addFeature('pikts', 8, 0);
-// map_handler.setDragCallback('onComplete', function(feature) { lonlat = feature.geometry.getBounds().getCenterLonLat().transform(
-//       map.getProjectionObject(),new OpenLayers.Projection("EPSG:4326")
-//     ); $('#picture_latitude').val(lonlat.lat); $('#picture_longitude').val(lonlat.lon); });
-
-
-// map_handler.addFeature('pikts', 50, 8, null);
-// map_handler.addFeature('pikts', 5000, 8000, null);
-
-// js :
-// var point = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(-111.04, 45.68), {icon:"icon.png"});
-// layer.addFeatures([point]);
-//
-// var point = new OpenLayers.Geometry.Point(ll.lon, ll.lat);
-// var feature = new OpenLayers.Feature.Vector(point,{icon:"icon.png"});
-// layer.addFeature(feature);
-//
-//
-// map_handler.setDragCallback('onComplete', function(feature) {
-//   lonlat = feature.geometry.getBounds().getCenterLonLat().transform(map.getProjectionObject(),new OpenLayers.Projection("EPSG:4326"));
-//   $('#picture_latitude').val(lonlat.lat);
-//   $('#picture_longitude').val(lonlat.lon);
-// });
-
-
-// map_handler.setCenterOnFeature(map_handler.getLayerFeatureById('pikts', 'picture_44'), 3);
-// map_handler.setDragCallback('onComplete', function (feat) { map_handler.setCenterOnFeature(feat, 5); });
-// map.setCenter(new OpenLayers.LonLat(1, 50), 5);
-// map.getLayersByName('pikts')[0].features[0]
-// pikts_handler.addFeaturePopup(map.getLayersByName('pikts')[0].features[0])
-// map.setCenter(map.getLayersByName('pikts2')[0].features[0].geometry.bounds.centerLonLat, 5)
-//
-// movestart, move, moveend, zoomend
-// map.events.register("moveend", map, function() {
-//            alert("panning");
-//        });
-// %{map}.events.register("moveend", map, function() {
-//   var center = %{map}.getCenter().clone().transform( %{map}.getProjectionObject(),new OpenLayers.Projection("EPSG:4326") );
-//   alert("moveend : " + center);
-//   %{map_handler}.addFeature('pikts', center.lat, center.lon);
-// });
-//
